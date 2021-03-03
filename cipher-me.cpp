6e5adde7b250dbf8bc032e7b9db3da9758783a6e
Raku <raul.r.h.rosas@gmail.com>
2021-03-03 14:39:40 -0800

    Minor details

--- cipher-me.cpp
+++ cipher-me.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
-#include <fstream>
 #include "Cipher/Cipher.h"
 int main(int argc, char**argv){
     if(argc < 2){
-       std::cout << "No file provided.\n" << std::endl; 
+       std::cout << "No file provided.\n"; 
        return -1;
     }else if(argc < 3){
-        std::cout << "No shift value provided.\n" << std::endl;
+        std::cout << "No shift value provided.\n";
     }
     Cipher cipher(argv[1], std::stoi(argv[2]));
-    std::string op = argv[3];
+    std::string op;
+    if(argc == 4){
+        op = argv[3];
+    }
     if(op == "-d"){
         cipher.decode();
         return 0;
@@ -17,7 +18,7 @@ int main(int argc, char**argv){
         cipher.encode();
         return 0;
     }else{
-        std::cout << "Too many arguments.\n" << std::endl;
+        std::cout << "Too many arguments.\n";
         return -1;
     }
 }
